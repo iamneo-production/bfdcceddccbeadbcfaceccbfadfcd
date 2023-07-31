2f5c3985e90006c9aa58378b4faeeff79452dba9
Iamneo <techadmin@iamneo.ai>
2023-07-31 18:12:25 +0000

    Backup Commit

--- angularapp/src/app/app.component.ts
+++ angularapp/src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+/*import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +7,36 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angularapp';
+}*/
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'pipes';
+  fromCurr :String = '';
+  toCurr   :string = '';
+  amount !:number;
+  result: string = 'sample';
+  show:boolean = false;
+
+
+  showResult()
+  {
+    console.log(this.fromCurr);
+    console.log(this.toCurr);
+    console.log(this.amount);
+    if(this.fromCurr !='' && this.toCurr != '')
+    {
+      this.show = true;
+    }
+    else
+    {
+      this.show = false;
+    }
+  }
 }
+
--- angularapp/src/app/app.module.ts
+++ angularapp/src/app/app.module.ts
@@ -1,4 +1,4 @@
-import { BrowserModule } from '@angular/platform-browser';
+/*import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
@@ -11,6 +11,31 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
+*/
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { FormsModule } from '@angular/forms';
+//import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { CurrencyconvPipe } from './currencyconv.pipe';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    CurrencyconvPipe
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule,
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
